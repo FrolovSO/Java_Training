fb4074296d94120b2a1b0d0e336c3a5875cd7f1f
Fralou_S <Frolovso92@yandex.ru>
2018-10-31 17:07:23 +0300

    Task3

--- Lesson2/Task3/src/Main.java
+++ Lesson2/Task3/src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+        //Task 3. Write a Java program to print the following grid
+        String[][] grid = {
+                {"_","_","_","_","_","_","_","_","_","_"},
+                {"_","_","_","_","_","_","_","_","_","_"},
+                {"_","_","_","_","_","_","_","_","_","_"},
+                {"_","_","_","_","_","_","_","_","_","_"}
+        };
 
-        System.out.println("Hello World!");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+
+
     }
 }
