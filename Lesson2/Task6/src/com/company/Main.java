91bf731c294a02876f704adf469410ba323b144e
Sergey <Frolovso92@yandex.ru>
2018-11-01 00:19:31 +0300

    task 6

--- Lesson2/Task6/src/com/company/Main.java
+++ Lesson2/Task6/src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Task 6.  Write a Java program to find the index of an array element.
+        int[] array;
+        array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*100);
+            System.out.print(" " + array[i]);
+        }
+        int Max = 0;
+        int Min = 0;
+        for (int i = 1; i < array.length; i++)
+        {
+            if (array[i] > array[Max])
+            {
+                Max = i;
+            }
+            else if (array[i] < array[Min])
+            {
+                Min = i;
+            }
+        }
+        System.out.println(" index Max value  " + "[" + Max + "]" + " " + "index Min value " + "[" + Min + "]");
     }
 }
