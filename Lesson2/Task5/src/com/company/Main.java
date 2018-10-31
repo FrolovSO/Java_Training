1bf52005cebeae0a3243e03d13c8db21e14dc586
Sergey <Frolovso92@yandex.ru>
2018-10-31 23:59:24 +0300

    Task5

--- Lesson2/Task5/src/com/company/Main.java
+++ Lesson2/Task5/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Task 5.  Write a Java program to test if an array contains a specific value.
+        int[] arrayForValue;
+        arrayForValue = new int[20];
+        for (int i = 0; i < arrayForValue.length; i++) {
+            arrayForValue[i] = (int)(Math.random()*100);
+            System.out.print(" " +arrayForValue[i]);
+        }
+        int specificValue = 99;
+        for (int j = 0; j < arrayForValue.length; j++){
+            if (arrayForValue[j] == specificValue){
+                System.out.println(" array contain a specific value " + arrayForValue[j]);
+            }
+
+        }
     }
 }
