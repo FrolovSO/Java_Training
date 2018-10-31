682bbb188c8b8f576937b9ee9f72bab32d860344
Sergey <Frolovso92@yandex.ru>
2018-10-31 23:05:30 +0300

    task 4

--- Lesson2/Task4/src/Main.java
+++ Lesson2/Task4/src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Task 4.  Write a Java program to calculate the average value of array elements.
+        int[] numbers = {5, 8, 12, 68, -57, 84, -35, 47, 37};
+
+        double average = 0;
+        if (numbers.length > 0)
+        {
+            double sum = 0;
+            for (int j = 0; j < numbers.length; j++) {
+                sum += numbers[j];
+            }
+            average = sum / numbers.length;
+        }
+        System.out.println(average);
+
     }
 }
