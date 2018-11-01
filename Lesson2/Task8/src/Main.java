00539afed9a17c0c52b01b99ce6c10a500f4bf34
Fralou_S <Frolovso92@yandex.ru>
2018-11-01 17:27:37 +0300

    Task 8

--- Lesson2/Task8/src/Main.java
+++ Lesson2/Task8/src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-
         //Task 8.  Write a Java program to copy an array by iterating the array.
-
+        int[] arrayFrom = new int[20];
+        for (int i = 0; i < arrayFrom.length; i++) {
+            arrayFrom[i] = (int)(Math.random()*10);
+            System.out.print(" " +arrayFrom[i]);
+        }
+        System.out.println(" ");
+        int arrayTo [] = new int [20];
+        System.arraycopy(arrayFrom, 0, arrayTo,0,20);
+        for (int i = 0; i <arrayTo.length; i++) {
+            System.out.print(" " + arrayTo[i]);
+        }
     }
 }
