06eb14aeead8f76519213372c87bb7139d471fed
Fralou_S <Frolovso92@yandex.ru>
2018-11-01 16:51:30 +0300

    Task7

--- Lesson2/Task7/src/Main.java
+++ Lesson2/Task7/src/Main.java
@@ -1,25 +1,22 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         //Task 7.  Write a Java program to remove a specific element from an array.
-
-        Scanner  s = new  Scanner(System.in);
-
-        ArrayList<Integer> arrayremove = new ArrayList<Integer>();
-
-        for(int i=0;i<10;i++) {
-            arrayremove.add(i);
-            System.out.print(arrayremove.get(i)+" ");
+        int[] arrayForValue;
+        arrayForValue = new int[20];
+        for (int i = 0; i < arrayForValue.length; i++) {
+            arrayForValue[i] = (int)(Math.random()*10);
+            System.out.print(" " +arrayForValue[i]);
         }
-        arrayremove.remove(5);
+        System.out.println(" ");
+        int specificValue = 1;
+        for (int j = 0; j < arrayForValue.length; j++){
+            if (arrayForValue[j] == specificValue) continue;
+                System.out.print(" " + arrayForValue[j]);
 
-        System.out.println();
-        for(int i=0;i<arrayremove.size();i++) {
 
-            System.out.print(arrayremove.get(i)+" ");
         }
 
     }
