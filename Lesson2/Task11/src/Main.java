a62ea1bb635b461c899ce7cd290d43af8bede6a4
Fralou_S <Frolovso92@yandex.ru>
2018-11-13 10:38:05 +0300

    Task11

--- Lesson2/Task11/src/Main.java
+++ Lesson2/Task11/src/Main.java
@@ -2,7 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
         //Task 11. Write a Java program to reverse an array of integer values.
-        System.out.println(" ");
+        int[] arrayForValue;
+        arrayForValue = new int[20];
+        
+        for (int i = 0; i < arrayForValue.length; i++) {
+            arrayForValue[i] = (int)(Math.random()*100);
+            System.out.print(" " +arrayForValue[i]);
+        }
+        System.out.println();
+        System.out.println(" reverse an array of integer values ");
+        for (int i = arrayForValue.length - 1; i >= 0; i--) {
+            System.out.print(" " + arrayForValue[i]);
+        }
+
 
     }
 }
